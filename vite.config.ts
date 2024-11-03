082a35d27de256c91601f84b981b6ce7043d49de
Matt Trombley <mattrtrombley@gmail.com>
2024-11-03 10:38:20 -0600

    Update site layout

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import compression from 'vite-plugin-compression'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    compression(), // Enables Gzip compression
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: '8 Bits Wiser',
+        short_name: '8BitWiser',
+        description: 'Technology consulting services specializing in AI/ML & web development',
+        theme_color: '#242424',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    target: 'esnext',
+    minify: 'terser',
+  }
+})
